f8af46a6e1ae454fbd3b7604da82a94d2acd14ce
Babongile Ncube <lbass1613@gmail.com>
2025-08-03 13:29:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const buildingRoutes = require('./routes/buildingRoutes');
 const coordinatesRoutes = require('./routes/coordinatesRoutes');
 const linksRoutes = require('./routes/linksRoutes');
+const routesRoutes = require('./routes/routesRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/buildings', buildingRoutes);
 app.use('/api/coordinates', coordinatesRoutes);
 app.use('/api/links', linksRoutes);
+app.use('/api/routes', routesRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
