25a707113a26dcc258982f974ad2910c90b59787
bassh331 <lbass1613@gmail.com>
2025-09-11 11:49:13 +0200

    fixing more

--- api/index.js
+++ api/index.js
@@ -3,3 +3,9 @@ const serverless = require("serverless-http");
 const app = require("../index"); // Your Express app
 
 module.exports = serverless(app);
+
+app.use((err, req, res, next) => {
+  console.error("❌ API Error:", err);
+  res.status(500).json({ error: "Internal Server Error", details: err.message });
+});
+
