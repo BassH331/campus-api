f163170a41ba2827e75fd7306bfaa52f69ca0009
bassh331 <lbass1613@gmail.com>
2025-09-11 05:54:51 +0200

    done

--- api/buildings.js
+++ api/buildings.js
@@ -1,7 +1,13 @@
-const app = require('../index'); // your Express app
-const { createServer } = require('http');
+// api/buildings.js
+const { connectToDatabase } = require('../db'); // your module
 
 module.exports = async (req, res) => {
-  const server = createServer(app);
-  server.emit('request', req, res);
+  try {
+    const db = await connectToDatabase();
+    const buildings = await db.collection('buildings').find({}).toArray();
+    res.status(200).json(buildings);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
